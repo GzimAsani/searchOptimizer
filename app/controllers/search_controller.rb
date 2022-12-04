70564cabfa09c13fc6f8d92c9a39b48f68c48dc6
Gzim Asani <gzimasanni@gmail.com>
2022-12-04 22:05:03 +0100

    fixed rubocop

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,10 +1,8 @@
 class SearchController < ApplicationController
-   skip_before_action :verify_authenticity_token  
+  skip_before_action :verify_authenticity_token
   def index
     @grouped_searches = Search.group(:query).pluck('query, count(query) as COUNT').sort_by do |a|
       a[1]
-       
     end.reverse.first(100)
-   
   end
 end
