dd2b40fa99b6d3aaf3a3b303359fcccfc31be627
Gzim Asani <gzimasanni@gmail.com>
2022-12-03 20:36:08 -0500

    fix: add migrations

---
+++ db/migrate/20221204013443_create_table.rb
@@ -0,0 +1,9 @@
+class CreateTable < ActiveRecord::Migration[7.0]
+  def change
+    create_table :tables do |t|
+      t.string :title
+      t.text :body
+      t.timestamps
+    end
+  end
+end
---
+++ db/migrate/20221204013512_searches.rb
@@ -0,0 +1,9 @@
+class Searches < ActiveRecord::Migration[7.0]
+  def change
+    create_table :searches do |t|
+      t.string :session_id
+      t.string :query
+      t.timestamps
+    end
+  end
+end
